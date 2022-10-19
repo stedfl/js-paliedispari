e9d02d8f89aa0ccf1d1e05e049e4e02173aa44ce
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-10-19 18:01:36 +0200

    add function for a game between user and pc
    function sum a number chosen by user and a random number for pc,
    if the user had chosen "odd" or "even" and the choice matches
    with the odd nature or even nature of the sum, the user wins
    otherwise pc wins.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const word = prompt("Scrivi una parola per controllare sia palindroma");
-document.querySelector(".output").innerText = palindromWord(word);
+document.querySelector(".output-palindromico").innerText = palindromWord(word);
 
 function palindromWord(word) {
   let revWord = "";
@@ -8,8 +8,27 @@ function palindromWord(word) {
   }
 
   if (word === revWord) {
-    return `La parola ${word} è palindroma`
+    return `La parola ${word} è palindroma`;
   } else {
-    return `La parola ${word} non è palindroma`
+    return `La parola ${word} non è palindroma`;
+  }
+}
+
+
+const dispPar = prompt("Scrivi pari o dispari").toLowerCase();
+const userNumb = parseInt(prompt("Scegli un numero da 1 a 5"));
+document.querySelector(".output-winner").innerText = gameNumber(dispPar, userNumb);
+
+function gameNumber(dispPar, userNumb) {
+  const pcNumb = Math.ceil(Math.random() * 5);
+  console.log("Il tuo numero è " + userNumb);
+  console.log("Il numero del pc è " + pcNumb);
+  const sum = userNumb + pcNumb;
+  console.log("la somma è " + sum);
+  console.log("Avevi scelto " + dispPar);
+  if ((sum % 2) && (dispPar === "dispari") || !(sum % 2) && (dispPar === "pari")) {
+    return "Hai vinto";
+  } else {
+    return "Hai perso, ha vinto il computer";
   }
 }
